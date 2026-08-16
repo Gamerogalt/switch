ab54e5ba994b64fb3aad45bc998e1f5193c608f3
Gamerogalt <278316904+Gamerogalt@users.noreply.github.com>
2026-08-16 20:06:07 +0530

    Add build.bat

---
+++ build.bat
@@ -0,0 +1,16 @@
+@echo off
+set CSC="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
+if not exist %CSC% (
+    echo C# compiler not found.
+    pause
+    exit /b 1
+)
+echo Compiling Switch...
+%CSC% /target:winexe /out:Switch.exe Switch.cs
+if %errorlevel% neq 0 (
+    echo Compilation failed.
+    pause
+    exit /b %errorlevel%
+)
+echo Compilation successful!
+echo You can now run Switch.exe
